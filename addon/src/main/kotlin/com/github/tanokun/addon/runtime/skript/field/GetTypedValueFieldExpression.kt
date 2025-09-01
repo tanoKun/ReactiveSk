816ff39719f244dc527a7a9b0b8413ca04618c4a
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-09-01 22:36:13 +0900

    refactor: フィールドとローカル変数アクセスロジックの最適化
    - `GetTypedValueFieldExpression` と `GetLocalTypedVariableExpression` クラスを `SimpleExpression` から `Expression` に変更。
    - 配列タイプの判定ロジックを追加し、`iterator` の実装を最適化して可読性を向上。
    - イベント処理や型変換に関連する各種メソッド (`getSingle`, `getArray` など) を統一的に再実装。
    - 不要な型チェックや条件を削除、コードを簡潔化。
    - 関連するメソッドやフィールドアクセスにおける新しい責務設計に対応。

--- addon/src/main/kotlin/com/github/tanokun/addon/runtime/skript/field/GetTypedValueFieldExpression.kt
+++ addon/src/main/kotlin/com/github/tanokun/addon/runtime/skript/field/GetTypedValueFieldExpression.kt
@@ -1,26 +1,25 @@
 package com.github.tanokun.addon.runtime.skript.field
 
 import ch.njol.skript.Skript
+import ch.njol.skript.classes.Changer
 import ch.njol.skript.lang.Expression
 import ch.njol.skript.lang.ExpressionType
 import ch.njol.skript.lang.SkriptParser
-import ch.njol.skript.lang.parser.ParserInstance
-import ch.njol.skript.lang.util.SimpleExpression
+import ch.njol.util.Checker
 import ch.njol.util.Kleenean
 import com.github.tanokun.addon.definition.Identifier
 import com.github.tanokun.addon.definition.dynamic.DynamicClass
-import com.github.tanokun.addon.definition.skript.dynamic.FunctionDefinitionInjector
-import com.github.tanokun.addon.definition.skript.dynamic.InitDefinitionInjector
 import com.github.tanokun.addon.intermediate.generator.fieldOf
 import com.github.tanokun.addon.intermediate.metadata.ModifierMetadata
-import com.github.tanokun.addon.lookup
+import com.github.tanokun.addon.runtime.DynamicAccessChecks.checkAccessError
 import org.bukkit.event.Event
 import java.lang.invoke.MethodHandle
+import java.lang.invoke.MethodHandles
 import java.lang.reflect.Field
-import java.lang.reflect.Modifier
 
 @Suppress("UNCHECKED_CAST")
-class GetTypedValueFieldExpression: SimpleExpression<Any>() {
+class GetTypedValueFieldExpression: Expression<Any> {
+    private val lookup = MethodHandles.publicLookup()
 
     companion object {
         init {
@@ -42,7 +41,9 @@ class GetTypedValueFieldExpression: SimpleExpression<Any>() {
 
     val targetClass: Class<*> get() = targetExpr.getReturnType()
 
-    private var returnType: Class<out Any>? = Any::class.java
+    private var returnType: Class<out Any> = Void.TYPE
+
+    private var isArray: Boolean = false
 
     private lateinit var getterHandle: MethodHandle
 
@@ -73,36 +74,64 @@ class GetTypedValueFieldExpression: SimpleExpression<Any>() {
             return false
         }
 
+        val modifiers = field.getAnnotation(ModifierMetadata::class.java).modifiers
+
+        if (clazz.checkAccessError(parser, modifiers, "Cannot read field '$fieldName' in '${clazz.simpleName}' because it is private field.")) return false
+
         returnType = field.type
-
-        if (Modifier.isPrivate(field.getAnnotation(ModifierMetadata::class.java).modifiers) && !isInThisClass(parser, clazz)) {
-            Skript.error("Cannot read field '$fieldName' in '${clazz.simpleName}' because it is private field.")
-            return false
-        }
-
+        isArray = returnType == ArrayList::class.java
         getterHandle = lookup.unreflectGetter(field)
 
         return true
     }
 
-    fun isInThisClass(parser: ParserInstance, type: Class<*>): Boolean {
-        val callerInInit = parser.getCurrentSections(InitDefinitionInjector::class.java).firstOrNull() ?: return false
-        if (callerInInit.thisDynamicClass == type) return true
-
-        val callerInFunction = parser.getCurrentSections(FunctionDefinitionInjector::class.java).firstOrNull() ?: return false
-        return callerInFunction.thisDynamicClass == type
-    }
-
-    override fun get(e: Event): Array<out Any>? {
-        val target = targetExpr.getSingle(e) ?: throw IllegalStateException("Integrity of '$targetExpr' is broken.")
-        val value = getterHandle.invoke(target) ?: throw IllegalStateException("Field '$fieldName' in '${targetExpr.returnType.simpleName}' is not initialized.")
-
-        return arrayOf(value)
-    }
-
     override fun getReturnType() = returnType
+
+    override fun getAnd(): Boolean = true
+
+    override fun setTime(time: Int): Boolean { return false }
+
+    override fun getTime(): Int = 0
+
+    override fun isDefault(): Boolean = false
+
+    override fun iterator(e: Event): Iterator<Any>? {
+        if (isArray) {
+            val list = getSingle(e) as ArrayList<*>
+            return list.filterNotNull().iterator()
+        }
+
+        return null
+    }
+
+    override fun isLoopOf(s: String): Boolean = false
+
+    override fun getSource(): Expression<*> = this
+
+    override fun simplify(): Expression<out Any> = this
+
+    override fun acceptChange(mode: Changer.ChangeMode?): Array<out Class<*>> = arrayOf()
+
+    override fun change(e: Event, delta: Array<out Any?>, mode: Changer.ChangeMode) { }
 
     override fun toString(e: Event?, debug: Boolean): String = "$targetExpr.$fieldName"
 
+    override fun getSingle(e: Event): Any {
+        val target = targetExpr.getSingle(e) ?: throw IllegalStateException("Integrity of '$targetExpr' is broken.")
+        val value = getterHandle.invoke(target) ?: throw IllegalStateException("Field '$fieldName' in '${targetExpr.returnType.simpleName}' is not initialized.")
+
+        return value
+    }
+
+    override fun getArray(e: Event): Array<out Any> = arrayOf(getSingle(e))
+
+    override fun getAll(e: Event): Array<out Any> = arrayOf(getSingle(e))
+
     override fun isSingle(): Boolean = true
+
+    override fun check(e: Event, c: Checker<in Any>, negated: Boolean): Boolean = true
+
+    override fun check(e: Event, c: Checker<in Any>?): Boolean = true
+
+    override fun <R : Any?> getConvertedExpression(vararg to: Class<R>): Expression<out R>? = null
 }
--- addon/src/main/kotlin/com/github/tanokun/addon/runtime/skript/variable/GetLocalTypedVariableExpression.kt
+++ addon/src/main/kotlin/com/github/tanokun/addon/runtime/skript/variable/GetLocalTypedVariableExpression.kt
@@ -1,22 +1,23 @@
 package com.github.tanokun.addon.runtime.skript.variable
 
 import ch.njol.skript.Skript
+import ch.njol.skript.classes.Changer
 import ch.njol.skript.lang.Expression
 import ch.njol.skript.lang.ExpressionType
 import ch.njol.skript.lang.SkriptParser
-import ch.njol.skript.lang.util.SimpleExpression
+import ch.njol.util.Checker
 import ch.njol.util.Kleenean
 import com.github.tanokun.addon.definition.Identifier
 import com.github.tanokun.addon.definition.variable.TypedVariableDeclaration
 import com.github.tanokun.addon.definition.variable.TypedVariableResolver
 import com.github.tanokun.addon.definition.variable.getDepth
 import com.github.tanokun.addon.definition.variable.getTopNode
-import com.github.tanokun.addon.runtime.variable.VariableFrames
+import com.github.tanokun.addon.runtime.variable.AmbiguousVariableFrames
 import org.bukkit.event.Event
 
 
 @Suppress("UNCHECKED_CAST")
-class GetLocalTypedVariableExpression: SimpleExpression<Any>() {
+class GetLocalTypedVariableExpression: Expression<Any> {
     companion object {
         init {
             Skript.registerExpression(
@@ -27,6 +28,8 @@ class GetLocalTypedVariableExpression: SimpleExpression<Any>() {
     }
 
     private lateinit var declaration: TypedVariableDeclaration
+
+    private var isArray: Boolean = false
 
     override fun init(
         exprs: Array<out Expression<*>>,
@@ -47,23 +50,16 @@ class GetLocalTypedVariableExpression: SimpleExpression<Any>() {
         val depth = node.getDepth()
         val topNode = node.getTopNode()
 
-        TypedVariableResolver.touchSection(topNode, depth, parser.currentSections.firstOrNull())
+        TypedVariableResolver.touchSection(topNode, depth, parser.currentSections.lastOrNull())
 
         declaration = TypedVariableResolver.getDeclarationInScopeChain(topNode, depth, variableName) ?: let {
             Skript.error("Typed variable '$variableName' is not declared in scope chain.")
             return false
         }
 
+        isArray = declaration.type == ArrayList::class.java
+
         return true
-    }
-
-    override fun get(e: Event): Array<out Any> {
-        val value = VariableFrames.get(e, declaration.index) ?: let {
-            throw IllegalStateException("Typed variable '${declaration.variableName}' is not initialized.")
-        }
-
-        return arrayOf(value)
-
     }
 
     override fun toString(e: Event?, debug: Boolean): String = "[${declaration.variableName} (${declaration.type.simpleName})]"
@@ -71,4 +67,45 @@ class GetLocalTypedVariableExpression: SimpleExpression<Any>() {
     override fun isSingle(): Boolean = true
 
     override fun getReturnType() = declaration.type
+
+    override fun getAnd(): Boolean = true
+
+    override fun setTime(time: Int): Boolean { return false }
+
+    override fun getTime(): Int = 0
+
+    override fun isDefault(): Boolean = false
+
+    override fun iterator(e: Event): Iterator<Any>? {
+        if (isArray) {
+            val list = getSingle(e) as ArrayList<*>
+            return list.filterNotNull().iterator()
+        }
+
+        return null
+    }
+
+    override fun isLoopOf(s: String): Boolean = false
+
+    override fun getSource(): Expression<*> = this
+
+    override fun simplify(): Expression<out Any> = this
+
+    override fun acceptChange(mode: Changer.ChangeMode?): Array<out Class<*>> = arrayOf()
+
+    override fun change(e: Event, delta: Array<out Any?>, mode: Changer.ChangeMode) { }
+
+    override fun getSingle(e: Event): Any = AmbiguousVariableFrames.get(e, declaration.index) ?: let {
+        throw IllegalStateException("Typed variable '${declaration.variableName}' is not initialized.")
+    }
+
+    override fun getArray(e: Event): Array<out Any> = arrayOf(getSingle(e))
+
+    override fun getAll(e: Event): Array<out Any> = arrayOf(getSingle(e))
+
+    override fun check(e: Event, c: Checker<in Any>, negated: Boolean): Boolean = true
+
+    override fun check(e: Event, c: Checker<in Any>?): Boolean = true
+
+    override fun <R : Any?> getConvertedExpression(vararg to: Class<R>): Expression<out R>? = null
 }
