7ce283d608b321e30b38100b4027864a0e42024d
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-10-05 17:43:21 +0900

    feature: Node の解析に関する Util

--- ReactiveSk-skript-v2_6_3/src/main/kotlin/com/github/tanokun/reactivesk/v263/skript/util/NodeUtils.kt
+++ ReactiveSk-skript-v2_6_3/src/main/kotlin/com/github/tanokun/reactivesk/v263/skript/util/NodeUtils.kt
@@ -1,8 +1,23 @@
-package com.github.tanokun.reactivesk.skriptadapter.common.skript
+package com.github.tanokun.reactivesk.v263.skript.util
 
 import ch.njol.skript.config.Node
 import ch.njol.skript.config.SectionNode
-import com.github.tanokun.reactivesk.compiler.frontend.analyze.ast.AstNode
+import ch.njol.skript.lang.TriggerItem
+import ch.njol.skript.lang.TriggerSection
+import com.github.tanokun.reactivesk.skriptadapter.common.reflection.Reflection
+
+/**
+ * [TriggerSection] から最初の [TriggerItem] を取得します。
+ *
+ * @receiver セクションオブジェクト
+ *
+ * @return セクション内の最初の [TriggerItem] または null
+ */
+fun TriggerSection.getFirstInSection(): TriggerItem? {
+    val field = Reflection.findField(this.javaClass, "first")
+
+    return field.get(this) as? TriggerItem
+}
 
 private val Node.shouldTerminateTraversal: Boolean
     get() = key?.startsWith("init") == true || key?.startsWith("function") == true
@@ -12,19 +27,21 @@ private val Node.shouldTerminateTraversal: Boolean
  *
  * このメソッドは現在のノードから親をたどり、以下の条件のいずれかに該当するまで遡ります：
  * - 親の親がnullになる (configに当たる)
- * - キーが`init`で始まる
- * - キーが`function`で始まる
+ * - キーが `init` で始まる
+ * - キーが `function` で始まる
+ *
+ * @receiver 起点となるノード
  *
  * @return 最上位のノード
  */
-fun Node.getTopNode(): AstNode.Struct<*> {
+fun Node.getTopNode(): Node {
     var current: Node = this
     while (current.parent?.parent != null) {
         if (current.shouldTerminateTraversal) break
         current.parent?.let { current = it }
     }
 
-    return AstNode.Struct(current, null)
+    return current
 }
 
 /**
@@ -32,6 +49,8 @@ fun Node.getTopNode(): AstNode.Struct<*> {
  *
  * このメソッドは現在のノードから親をたどり、ルートに到達するまでの
  * 階層数をカウントします。
+ *
+ * @receiver 深さを計算する起点のノード
  *
  * @return ルートノードまでのスコープ数
  */
