f4c05356f64bc1fa82a02234c4dc782197afc125
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-10-06 18:27:20 +0900

    feature: v2.7.0以上の基底クラス

---
+++ ReactiveSk-skript-v2_7_0/build.gradle.kts
@@ -0,0 +1,89 @@
+plugins {
+    kotlin("jvm")
+    `java-library`
+    alias(libs.plugins.bukkit.yaml)
+    alias(libs.plugins.shadow)
+    alias(libs.plugins.antlr)
+}
+
+group = "com.github.tanokun.reactivesk.v263"
+version = "Developing-1-v2.7.0"
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://repo.skriptlang.org/releases")
+    maven("https://repo.songoda.com/repository/public/")
+
+    maven {
+        url = uri("https://maven.pkg.github.com/tanoKun/ReactiveSk-Common")
+        credentials {
+            username = System.getenv("GITHUB_ACTOR")
+            password = System.getenv("GITHUB_TOKEN")
+        }
+
+        content {
+            includeGroup("com.github.tanokun.reactivesk")
+        }
+    }
+}
+
+extra["reactive.jvmToolchain"] = 17
+apply(plugin = "com.tanokun.reactive-convention")
+
+tasks.named<AntlrTask>("generateGrammarSource") {
+    outputDirectory = file("build/generated-src/antlr/main")
+    arguments = arguments + listOf("-package", "com.github.tanokun.addon", "-visitor")
+}
+
+tasks {
+    val copyJarToPlugins by registering(Copy::class) {
+        dependsOn(shadowJar)
+        from(shadowJar.get().archiveFile)
+        into("C:/Users/owner/Desktop/1.17.1 paper/plugins")
+    }
+
+    shadowJar {
+        dependsOn(subprojects.map { it.tasks.named("test") })
+
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+        archiveBaseName.set("ReactiveSk")
+        archiveVersion.set("developing")
+
+        dependencies {
+            exclude(dependency("com.ibm.icu:icu4j"))
+        }
+
+        mergeServiceFiles()
+        finalizedBy(copyJarToPlugins)
+    }
+}
+
+dependencies {
+    compileOnly(libs.spigot)
+    compileOnly(libs.skript.api.v270)
+
+    implementation(project(":ReactiveSk-skript-v2_6_3"))
+
+    implementation(libs.bundles.mccoroutine)
+    implementation(libs.kotlinx.coroutines)
+
+    implementation(libs.bytebuddy.agent)
+    implementation(libs.bytebuddy)
+
+    implementation(libs.bundles.reactivesk.all)
+
+    testImplementation(kotlin("test"))
+}
+
+bukkit {
+    main = "com.github.tanokun.reactivesk.v270.ReactiveSkAddon"
+    name = "ReactiveSk"
+
+    apiVersion = "1.13"
+
+    depend = listOf("Skript")
+    version = project.version.toString()
+}
